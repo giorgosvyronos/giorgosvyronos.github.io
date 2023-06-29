5a1cfe4e079615c7cc2e83f5afe0a7cbe2704fee
Giorgos Vyronos <giorgos.vyronos19@imperial.ac.uk>
2023-06-29 17:19:14 +0100

    updates

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,7 +9,7 @@ function Portfolio() {
       <div className= "w-w-full md:w-9/12" >
          <Title>Projects</Title>
          <div class="slider w-w-full md:w-9/12">
-            <div class="slides w-w-full md:w-9/12">
+            <div class="slides">
          {portfolio.map(project => (
                   <PortfolioItem 
                      imgUrl={project.imgUrl}
